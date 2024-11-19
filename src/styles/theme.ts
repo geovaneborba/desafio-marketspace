8a4d69e2adf073661f0e347f993cfacbc94a6da2
Geovane Borba <geovanne@live.com>
2024-11-19 00:27:35 -0300

    chore(styled-components): add custom theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+export const theme = {
+  colors: {
+    blue: '#364D9D',
+    'blue-light': '#647AC7',
+    'red-light': '#EE7979',
+    'gray-1': '#1A181B',
+    'gray-2': '#3E3A40',
+    'gray-3': '#5F5B62',
+    'gray-4': '#9F9BA1',
+    'gray-5': '#D9D8DA',
+    'gray-6': '#EDECEE',
+    'gray-7': '#F7F7F8',
+  },
+
+  fontFamily: {
+    regular: 'Karla-Regular',
+    bold: 'Karla-Bold',
+  },
+
+  fontSize: {
+    xs: '10px',
+    sm: '12px',
+    md: '14px',
+    base: '16px',
+    lg: '18px',
+    xl: '20px',
+    '2xl': '24px',
+    '4xl': '32px',
+  },
+}
