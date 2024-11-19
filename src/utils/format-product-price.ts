68969da2dc363b72c4f729d7451b5e8c905006f3
Geovane Borba <geovanne@live.com>
2024-11-19 19:09:05 -0300

    feat(utils): add utility function to format product price

---
+++ src/utils/format-product-price.ts
@@ -0,0 +1,19 @@
+export function formatToBRLCurrency(number: number) {
+  return new Intl.NumberFormat('pt-BR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+    minimumSignificantDigits: 2,
+  }).format(number)
+}
+
+export function convertTextToCurrency(value: string) {
+  if (value.trim() === '') {
+    return undefined
+  }
+
+  const onlyNumber = '0000' + value.replace(/\D/g, '').slice(0, 10)
+
+  const numberFormatted = onlyNumber.slice(0, -2) + '.' + onlyNumber.slice(-2)
+
+  return parseFloat(numberFormatted)
+}
